760538db93c43de99f197909578f654df9a8739b
jackhkmatthews <jackhkmatthews@gmail.com>
2016-09-07 19:15:03 +0100

    sibling scopes

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,24 @@
 angular.module("todoListApp", [])
+
 .controller('mainCtrl', function($scope) {
   $scope.helloWorld = function(){
     console.log("hello there! This is the helloWorld controler function in the mainCtrl");
   };
-});
+})
+
+.controller('coolCtrl', function($scope){
+  $scope.whoAmI = function(){
+    console.log("hello there, this is the coolCtrl function!");
+  };
+
+  $scope.helloWorld = function() {
+    console.log("this is not the main ctrl!");
+  };
+
+})
+
+.controller('imASibling', function($scope){
+
+  $scope.foober = 1234;
+
+})
