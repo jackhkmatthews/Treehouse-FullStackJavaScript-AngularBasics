e091ce75d091352ccc1eed79d0e452c6a80b68e1
jackhkmatthews <jackhkmatthews@gmail.com>
2016-09-11 18:35:27 +0100

    using services to save and delete create data

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,27 @@
 angular.module("todoListApp", [])
 
 .controller('mainCtrl', function($scope, dataService) {
-  $scope.learningNgChange = function(){
-    console.log("input changed");
+  $scope.addTodo = function() {
+    var todo = {name: "This is a new todo."};
+    $scope.todos.push(todo);
   };
-  
+
   $scope.helloWorld = dataService.helloWorld;
 
   dataService.getTodos(function(response){
     console.log(response.data)
     $scope.todos = response.data;
   });
+
+  $scope.deleteTodo = function(todo, $index) {
+    dataService.deleteTodo(todo);
+    $scope.todos.splice($index, 1);
+  }
+
+  $scope.saveTodo = function(todo) {
+    dataService.saveTodo(todo);
+  }
+
 })
 .service('dataService', function($http){
   this.helloWorld = function(){
@@ -19,5 +30,13 @@ angular.module("todoListApp", [])
   this.getTodos = function(callback){
     $http.get('mock/todos.json')
     .then(callback)
-  }
+  };
+
+  this.deleteTodo = function(todo){
+    console.log("the" + todo.name + " has been deleted!");
+  };
+
+  this.saveTodo = function(todo){
+    console.log("the" + todo.name + " todo has been saved");
+  };
 });
